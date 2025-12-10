f9c3b26396dff76a7735314cda5b52a2239bc701
Esdedev <145047482+esdedev@users.noreply.github.com>
2025-12-10 17:52:47 +0100

    Add biome and drizzle

---
+++ src/drizzle/db.ts
@@ -0,0 +1,7 @@
+// import { drizzle } from "drizzle-orm/node-postgres"
+import { drizzle } from "drizzle-orm/pglite"
+import { env } from "@/data/env/server"
+// import * as schema from "@/drizzle/schema"
+
+// export const db = drizzle(env.DATABASE_URL, { schema })
+export const db = drizzle({ connection: { dataDir: "./database" } })
